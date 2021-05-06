d28052249eb901be13b765d37e3c3e42e81c0178
Lamer Matrixxx <jssk383@gmail.com>
2021-05-06 16:31:44 +0300

    допилив рандом

--- Control_oop/Program.cs
+++ Control_oop/Program.cs
@@ -7,19 +7,20 @@ namespace Control_oop
     {
         static void Main(string[] args)
         {
-            int[,] arr = {
-                { 68, 62, 56, 37, 36, 60, 52, 64, 50 }, //
-                { 66, 50, 36, 54, 41, 36, 64, 42, 46 }, //
-                { 35, 52, 45, 44, 52, 46, 54, 62, 53 }, // 
-                { 36, 52, 64, 41, 62, 36, 54, 42, 56 }, // 
-                { 46, 55, 66, 64, 37, 26, 64, 52, 42 }, // Двохвимірний масив з випадковими числами
-                { 66, 35, 46, 54, 50, 56, 65, 42, 36 }, // 
-                { 68, 55, 36, 44, 62, 46, 36, 65, 66 }, //
-                { 37, 42, 66, 44, 62, 36, 44, 62, 36 }, // 
-                { 69, 62, 56, 44, 42, 36, 54, 32, 56 }, //
-            };
-            var max = Enumerable.Range(0, arr.GetLength(0)).Select(e => arr[e, e]).Max(); // знаходження максимального числа
-            var min = Enumerable.Range(0, arr.GetLength(0)).Select(e => arr[e, e]).Min(); // знаходження мінімального числа
+            int n = 9;
+            int[,] arr = new int[n, n];
+            Random r = new Random();        //заповнюємо матрицю випадковими числами
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i, j] = r.Next(35, 70);    //кожен елемент приймає значення від 35 до 70
+                    Console.Write("{0} ", arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            var max = Enumerable.Range(0, arr.GetLength(0)).Select(n => arr[n, n]).Max(); // знаходження максимального числа
+            var min = Enumerable.Range(0, arr.GetLength(0)).Select(n => arr[n, n]).Min(); // знаходження мінімального числа
 
             Console.WriteLine("Matrix:");
             for (int i = 0; i < arr.GetLength(0); i++)
